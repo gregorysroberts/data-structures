3dc6a0298123b21aca08c2d2d12a9c8e22766e44
Greg Roberts <gregorysroberts@gmail.com>
2014-01-23 20:07:53 -0700

    Adds names to array as they come in

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ var users_joined = [];
 var chatrooms = {'west_side':{}, 'east_side':{}};
 
 window.new_user_joined = function(name) {
+	users_joined.push(name);
+	console.log(users_joined);
 }
 
 //this function is only called once by the server every five seconds
