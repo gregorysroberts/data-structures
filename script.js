c8c5ea0aa53ae683c53e99bf89fef258975df05b
Greg Roberts <gregorysroberts@gmail.com>
2014-01-23 20:53:39 -0700

    Finishes project

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 var users_joined = [];
 var chatrooms = {'west_side':{}, 'east_side':{}};
+var west = true;
 
 window.new_user_joined = function(name) {
 	users_joined.push(name);
-	console.log(users_joined);
 }
 
 //this function is only called once by the server every five seconds
 window.assign_to_chatroom = function() {
 	//add this element for every user assigned to a chatroom: <p><button type="button" class="btn btn-warning btn-xs">[[name]]</button></p>
-    console.log("assigning users to rooms");
-}
+    if (west) {
+    	var name = users_joined.pop();
+    	$('#west').append("<p><button type='button' class='btn btn-warning btn-xs'>" + name + "</button></p>");
+    	west=false;
+    } else {
+    	var name = users_joined.pop();
+    	$('#east').append("<p><button type='button' class='btn btn-warning btn-xs'>" + name + "</button></p>");
+    	west=true;
+    }
+    console.log("assigning users to room");
+ }
